5194c46349bed259b402b79bbcf9f6c41ff9606a
Davide Perozzi <myself@davideperozzi.com>
2023-08-05 22:25:07 +0200

    feat: reverted to dynamic import with "file://"

--- core/sencha.ts
+++ core/sencha.ts
@@ -185,7 +185,7 @@ export class Sencha {
     }
 
     this._configPath = this.path(configFile);
-    const configModule = await import(this._configPath);
+    const configModule = await import('file://' + this._configPath);
     const partials = Object.values(configModule);
 
     for (const options of partials) {
